c8c31cf2d0e2342dafe1a9def8ee42ddd93828fc
Prateek Kumar <prateek@prateekkumar.in>
2020-03-15 17:40:36 +0530

    Initial implementation of TOTP/HOTP
    Signed-off-by: Prateek Kumar <prateek@prateekkumar.in>

---
+++ src/main.rs
@@ -0,0 +1,56 @@
+use crypto::hmac::Hmac;
+use crypto::mac::Mac;
+use crypto::sha1::Sha1;
+use std::time::{SystemTime, UNIX_EPOCH};
+
+fn decode(code: u8) -> u8 {
+    match code.to_ascii_uppercase() {
+        b'A'..=b'Z' => code - b'A',
+        b'2'..=b'7' => code - b'2' + 26,
+        _ => panic!("Incorrect code"),
+    }
+}
+
+fn parse_key(key: &str) -> Vec<u8> {
+    let mut buffer: u64 = 0;
+    let mut bits_left = 0;
+    let mut result = vec![];
+    let key = key
+        .as_bytes()
+        .iter()
+        .filter(|&c| *c != b' ')
+        .map(|&c| decode(c));
+    for c in key {
+        buffer <<= 5;
+        buffer |= (c & 31) as u64;
+        bits_left += 5;
+        if bits_left >= 8 {
+            result.push((buffer >> (bits_left - 8)) as u8);
+            bits_left -= 8;
+        }
+    }
+    result
+}
+
+fn truncate(hash: &[u8]) -> u64 {
+    let offset = (hash[19] & 0xf) as usize;
+    let truncated: u64 = (hash[offset] as u64 & 0x7f) << 24
+        | (hash[offset + 1] as u64 & 0xff) << 16
+        | (hash[offset + 2] as u64 & 0xff) << 8
+        | (hash[offset + 3] as u64 & 0xff);
+    truncated % 1_000_000
+}
+
+fn main() {
+    let now = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_secs()
+        / 30;
+    let sha1 = Sha1::new();
+    let key = parse_key("aaaa bbbb cccc xxxx yyyy zzzz 2222 7777");
+    let mut hmac = Hmac::new(sha1, &key);
+    hmac.input(&now.to_be_bytes());
+    let otp = truncate(hmac.result().code());
+    println!("{:03} {:03}", otp / 1000, otp % 1000);
+}
